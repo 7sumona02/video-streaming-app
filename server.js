e9af3cc6eddb12d804fb9884f812bfcd97ce1ab9
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-05-11 16:47:28 +0530

    added

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ nextApp.prepare().then(() => {
         socket.on('join-room', (roomId, userId) => {
             socket.join(roomId);
             socket.to(roomId).emit('user-connected', userId);
+            
+            socket.on('disconnect', () => {
+                socket.to(roomId).emit('user-disconnected', userId);
+            });
         });
     });
 
